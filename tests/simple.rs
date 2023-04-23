112b7bf86d71660fdec03f86f1e9865df45ad035
Sylvestre Ledru <sylvestre@debian.org>
2023-04-23 22:10:56 +0200

    improve the coverage

--- tests/simple.rs
+++ tests/simple.rs
@@ -82,16 +82,23 @@ fn test_duration_parsing() {
     assert_eq!(from_str("day").unwrap(), Duration::seconds(86_400));
 
     assert_eq!(from_str("1 hour").unwrap(), Duration::seconds(3_600));
+    assert_eq!(from_str("1 h").unwrap(), Duration::seconds(3_600));
     assert_eq!(from_str("1 hour ago").unwrap(), Duration::seconds(-3_600));
     assert_eq!(from_str("-2 hours").unwrap(), Duration::seconds(-7_200));
     assert_eq!(from_str("hour").unwrap(), Duration::seconds(3_600));
 
     assert_eq!(from_str("1 minute").unwrap(), Duration::seconds(60));
+    assert_eq!(from_str("1 min").unwrap(), Duration::seconds(60));
     assert_eq!(from_str("2 minutes").unwrap(), Duration::seconds(120));
+    assert_eq!(from_str("2 mins").unwrap(), Duration::seconds(120));
+    assert_eq!(from_str("2m").unwrap(), Duration::seconds(120));
     assert_eq!(from_str("min").unwrap(), Duration::seconds(60));
 
     assert_eq!(from_str("1 second").unwrap(), Duration::seconds(1));
+    assert_eq!(from_str("1 s").unwrap(), Duration::seconds(1));
     assert_eq!(from_str("2 seconds").unwrap(), Duration::seconds(2));
+    assert_eq!(from_str("2 secs").unwrap(), Duration::seconds(2));
+    assert_eq!(from_str("2 sec").unwrap(), Duration::seconds(2));
     assert_eq!(from_str("sec").unwrap(), Duration::seconds(1));
 
     assert_eq!(from_str("now").unwrap(), Duration::ZERO);
