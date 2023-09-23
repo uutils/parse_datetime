62345bff9240b0eab723c038a448823df21cea79
Sylvestre Ledru <sylvestre@debian.org>
2023-09-23 10:49:17 +0200

    add examples to make sure we can use them properly

--- tests/simple.rs
+++ tests/simple.rs
@@ -1,5 +1,8 @@
-use chrono::{Duration, Local,Weekday};
-use parse_datetime::{parse_datetime_at_date, parse_relative_time::parse_relative_time, parse_timestamp::parse_timestamp, parse_weekday::parse_weekday};
+use chrono::{Duration, Local, Weekday};
+use parse_datetime::{
+    parse_datetime_at_date, parse_relative_time::parse_relative_time,
+    parse_timestamp::parse_timestamp, parse_weekday::parse_weekday,
+};
 
 #[test]
 fn test_parse_datetime_at_date() {
@@ -23,7 +26,6 @@ fn test_parse_timestamp() {
     let ts = parse_timestamp("@1234").unwrap();
     assert_eq!(ts, 1234);
 }
-
 
 #[test]
 fn test_weekday() {
