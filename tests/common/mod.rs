937572d5578b5ef01ed9b6ef70d08fb66317245b
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2025-06-24 14:43:56 +0200

    tests: add license header to integration tests

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,3 +1,6 @@
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
+
 use chrono::{DateTime, FixedOffset};
 use parse_datetime::{parse_datetime, parse_datetime_at_date};
 
