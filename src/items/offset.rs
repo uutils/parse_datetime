0c09d5bd173204fa93bc23799351a7f7f242552d
yuankunzhang <fmcocoa@gmail.com>
2025-09-23 22:54:00 +0800

    refactor: rename the timezone module to offset

--- src/items/offset.rs
+++ src/items/offset.rs
@@ -1,7 +1,7 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
-//! Parse a timezone item.
+//! Parse an offset item.
 //!
 //! From the GNU docs:
 //!
